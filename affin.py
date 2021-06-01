2c3e1403b8c89709e2c1bc6c2a3bb063c6415079
kl <datesaver1234@gmail.com>
2021-06-02 00:52:27 +0900

    fin stretch

--- affin.py
+++ affin.py
@@ -6,9 +6,18 @@ np.set_printoptions(threshold=1000000)
 
 
 def stretch(image):
-    affin_array = np.array([[0.3, 0, 0], [0, 1, 0], [0, 0, 1]])
+    origin_size, _ = image.size
+    y_size = 0.9
+    x_size = 0.9
+    y_position = origin_size * (1.0 - y_size)
+    x_position = origin_size * (1.0 - y_size)
+    stretch_array = np.array([[y_size, 0, 0], [0, x_size, 0], [0, 0, 1]])
+    position_array = np.array(
+        [[1, 0, y_position], [0, 1, x_position], [0, 0, 1]])
+    affin_array = np.matmul(stretch_array, position_array)
     affine_data = tuple(np.linalg.inv(affin_array).flatten())
-    image_stretched = image.transform(image.size, Image.AFFINE, affine_data)
+    image_stretched = image.transform(
+        image.size, Image.AFFINE, affine_data, fill=1, fillcolor=255)
     return image_stretched
 
 
