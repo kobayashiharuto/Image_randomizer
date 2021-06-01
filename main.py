c3927f9600638881a805c7647ae3e6d5876aa5bb
kl <datesaver1234@gmail.com>
2021-06-02 02:20:01 +0900

    指定したディレクトリの全ての画像から画像生成できるように

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from PIL import Image
 from controller import randomized_image_generate
+import glob
+
+
+def get_file_paths(path: str):
+    files = glob.glob(path + '/*')
+    return files
 
 
 def main():
-    image = Image.open('data/9_0.png')
-    randmized_images = randomized_image_generate(image)
+    paths = get_file_paths('data/origin_images')
 
-    for index, image in enumerate(randmized_images):
-        image.save(f'out/9_{index}.png')
+    for path in paths:
+        image = Image.open(path)
+        randmized_images = randomized_image_generate(image)
+
+        name = path.split('\\')[-1].split('.')[0]
+        for index, image in enumerate(randmized_images):
+            image.save(f'out/train_data1/{name}_{index}.png')
 
 
 if __name__ == '__main__':
