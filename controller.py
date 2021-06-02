01b5ea8853fc6a8c18acdbff943e7de616a2e5de
kl <datesaver1234@gmail.com>
2021-06-02 20:25:05 +0900

    random file

--- controller.py
+++ controller.py
@@ -1,30 +1,10 @@
-from PIL import Image
 from tools.move import move_image
 from tools.noise import noise_image
 from tools.stretch import stretch_image
 from tools.binary_converter import binary_convert
-import random
 
 
-# 28*28サイズの時
-def random28():
-    size_y = random.uniform(0.7, 1.1)
-    size_x = random.uniform(0.7, 1.1)
-    move_y = random.randint(-2, 2)
-    move_x = random.randint(-2, 2)
-    return size_y, size_x, move_y, move_x
-
-
-# 200*200サイズの時
-def random200():
-    size_y = random.uniform(0.7, 1.1)
-    size_x = random.uniform(0.7, 1.1)
-    move_y = random.randint(-20, 20)
-    move_x = random.randint(-20, 20)
-    return size_y, size_x, move_y, move_x
-
-
-def randomized_image_generate(image, resize, count):
+def randomized_image_generate(image, resize, count, random):
     image_binary = binary_convert(
         image,
         resize=resize,
@@ -33,7 +13,7 @@ def randomized_image_generate(image, resize, count):
 
     images = []
     for _ in range(count):
-        size_y, size_x, move_y, move_x = random200()
+        size_y, size_x, move_y, move_x = random
         image_randomized = noise_image(image_binary)
         image_stretched = stretch_image(image_randomized, size_y, size_x)
         image_moved = move_image(image_stretched, move_y, move_x)
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 from PIL import Image
 from controller import randomized_image_generate
 import glob
@@ -8,13 +9,32 @@ def get_file_paths(path: str):
     return files
 
 
+# 28*28サイズの時
+def random28():
+    size_y = random.uniform(0.7, 1.1)
+    size_x = random.uniform(0.7, 1.1)
+    move_y = random.randint(-2, 2)
+    move_x = random.randint(-2, 2)
+    return size_y, size_x, move_y, move_x
+
+
+# 200*200サイズの時
+def random200():
+    size_y = random.uniform(0.7, 1.1)
+    size_x = random.uniform(0.7, 1.1)
+    move_y = random.randint(-20, 20)
+    move_x = random.randint(-20, 20)
+    return size_y, size_x, move_y, move_x
+
+
 def main():
     paths = get_file_paths('data/origin_images')
 
     for path in paths:
         image = Image.open(path)
         randmized_images = randomized_image_generate(
-            image, resize=200, count=5)
+            image, resize=200, count=5, random=random200()
+        )
 
         name = path.split('\\')[-1].split('.')[0]
         for index, image in enumerate(randmized_images):
