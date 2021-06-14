13e3d6d1bf543d4f0270bf9922c8bbf145ba4b0d
kl <datesaver1234@gmail.com>
2021-06-15 04:44:58 +0900

    v7start

--- main.py
+++ main.py
@@ -35,42 +35,25 @@ def random200():
     return size_y, size_x, move_y, move_x, rotate, strength, move
 
 
-# 100*100サイズの時
-def random100():
-    size_y = random.uniform(0.6, 1.1)
-    size_x = random.uniform(0.6, 1.1)
-    move_y = random.randint(-10, 10)
-    move_x = random.randint(-10, 10)
-    rotate = random.uniform(-np.pi/36, np.pi/36)
-    strength = 6
-    move = 7
-    return size_y, size_x, move_y, move_x, rotate, strength, move
-
-
-def image_randomize(multi, alpha):
-    paths = get_file_paths('data/icons')
+def main():
+    paths = get_file_paths(
+        r'C:\Users\owner\Desktop\icon_for_ML\arc\20class\ios_train2')
     file_count = 500
 
     for path in paths:
         image = Image.open(path)
-        if alpha:
-            image = image.convert('RGBA')
-            new_image = Image.new('RGBA', image.size, 'WHITE')
-            new_image.paste(image, (0, 0), image)
-            new_image = new_image.convert('RGB')
-            image = new_image
         randmized_images = randomized_gray_image_generate(
-            image, resize=50, count=file_count, random=random100
+            image, resize=50, count=file_count, random=random200
         )
 
         file_name = path.split('\\')[-1].split('.')[0]
         image_category = file_name.split('_')[0]
+        file_index = int(file_name.split('_')[1])
 
         for index, image in enumerate(randmized_images):
-            if multi:
-                index = index + int(file_name.split('_')[1]) * file_count
-            image.save(f'out/train_datav6/{image_category}_{index}.png')
+            new_index = index + file_index * file_count
+            image.save(f'out/train_datav7/{image_category}_{new_index}.png')
 
 
 if __name__ == '__main__':
-    image_randomize(multi=False, alpha=True)
+    main()
